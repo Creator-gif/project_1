83ead42f9bdf3cb6f038ac616a6be75b3452f54f
Арсен <arsen.beishalyev@gmail.com>
2025-06-16 22:13:36 +0600

    Создал перенаправления при входе и выходе

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 
-# Create your views here.
+class CustomLoginView(LoginView):
+    template_name = 'users/login.html'  # Используем этот шаблон
+    redirect_authenticated_user = True  # Если уже вошёл - перенаправит на главную
+
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('home')  # После выхода - на главную
