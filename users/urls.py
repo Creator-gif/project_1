f807b9761390e7a0503b6cbdc2292899fdc66373
Арсен <arsen.beishalyev@gmail.com>
2025-06-21 15:22:19 +0600

    Очистка лишних файлов из индекса, применён .gitignore. Исправил работу входа и выхода

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from . import views
 from .views import CustomLoginView, CustomLogoutView
 
 urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
+    path('login/', views.user_login, name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 class CustomLoginView(LoginView):
     template_name = 'users/login.html'  # Используем этот шаблон
@@ -7,3 +9,15 @@ class CustomLoginView(LoginView):
 
 class CustomLogoutView(LogoutView):
     next_page = reverse_lazy('home')  # После выхода - на главную
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('templates/home.html')  # или другой маршрут
+        else:
+            return render(request, 'users/login.html', {'error': 'Неверные данные'})
+    return render(request, 'users/login.html')
