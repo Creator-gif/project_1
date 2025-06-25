c943f42c9f0f9193986678ee8b04c100001e2d03
Арсен <arsen.beishalyev@gmail.com>
2025-06-25 14:49:41 +0600

    Исправил прошлую ошибку. Добавил личные кабинеты.

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-from .views import CustomLoginView, CustomLogoutView
+from .views import CustomLoginView, CustomLogoutView, StudentProfileView, TeacherProfileView
 
 urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),  # Добавьте эту строку
+    path('student_profile/', StudentProfileView.as_view(), name='student-profile'),
+    path('teacher_profile/', TeacherProfileView.as_view(), name='teacher-profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -3,6 +3,10 @@ from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import UserRegistrationForm
+from django.db.models import Avg, Count
+from assignments.models import Submission, Assignment
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import TemplateView
 
 class CustomLoginView(LoginView):
     template_name = 'users/login.html'  # Используем этот шаблон
@@ -34,3 +38,49 @@ def register(request):
     else:
         form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form': form})
+
+class StudentProfileView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
+    template_name = 'users/student_profile.html'
+    
+    def test_func(self):
+        return self.request.user.is_student
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        student = self.request.user
+        
+        # Статистика
+        submissions = Submission.objects.filter(student=student).select_related('assignment')
+        completed = submissions.count()
+        avg_grade = submissions.aggregate(avg=Avg('grade'))['avg'] or 0
+        
+        context.update({
+            'submissions': submissions,
+            'completed_assignments': completed,
+            'avg_grade': round(avg_grade, 1),
+            'late_submissions': submissions.filter(is_late=True).count()
+        })
+        return context
+    
+class TeacherProfileView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
+    template_name = 'users/teacher_profile.html'
+    
+    def test_func(self):
+        return self.request.user.is_teacher
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        teacher = self.request.user
+        
+        assignments = Assignment.objects.filter(created_by=teacher)
+        submissions = Submission.objects.filter(assignment__in=assignments)
+        
+        context.update({
+            'assignments': assignments,
+            'ungraded_submissions': submissions.filter(grade__isnull=True).count(),
+            'active_assignments': assignments.filter(status='published').count(),
+            'top_students': submissions.values('student').annotate(
+                avg_grade=Avg('grade')
+            ).order_by('-avg_grade')[:5]
+        })
+        return context
